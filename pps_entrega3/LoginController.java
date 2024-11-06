862f788b39051cc66bb20587b459f9d989113f47
Etamayor <etamayor1@eafit.edu.co>
2024-11-06 10:25:21 -0500

    Update LoginController.java

--- pps_entrega3/LoginController.java
+++ pps_entrega3/LoginController.java
@@ -1,24 +1,22 @@
 package sample.pps_entrega3;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
     @FXML
-    private TextField emailOrUsernameField;
+    private TextField usernameField;
 
     @FXML
     private TextField passwordField;
-
-    @FXML
-    private CheckBox rememberMeCheckbox;
-
-    @FXML
-    private Text forgotPasswordText;
 
     @FXML
     private Button loginButton;
@@ -27,5 +25,19 @@ public class LoginController {
     private Button signUpButton;
 
     @FXML
-    public void initialize() {}
+    private void handleSignUp() throws IOException {
+        // Correcta carga del archivo FXML desde el paquete sample/pps_entrega3
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/pps_entrega3/Signup.fxml"));
+        if (loader.getLocation() == null) {
+            throw new IllegalStateException("Location of Signup.fxml is not set or incorrect");
+        }
+        Parent signUpRoot = loader.load();
+
+        // Obtener el Stage actual (la ventana) y cambiar la escena
+        Stage stage = (Stage) signUpButton.getScene().getWindow();
+        stage.setScene(new Scene(signUpRoot));
+        stage.show();
+    }
+
+    // Otros métodos del controlador
 }
