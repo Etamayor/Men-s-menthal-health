b3b0d17883cf8a270287979ef8cc785f3ccbba58
Etamayor <etamayor1@eafit.edu.co>
2024-11-13 10:23:31 -0500

    Update Main.java

--- pps_entrega3/Main.java
+++ pps_entrega3/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample/pps_entrega3/Login.fxml"));
         primaryStage.setTitle("Login");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, 903, 600));
         primaryStage.show();
     }
 
